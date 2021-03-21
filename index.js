b114a8834eda66628b4f4288fad9ed2828a0ad7e
ErikHendrikson <erikhendrikson@hotmail.com>
2021-03-21 10:26:26 +0100

    all files

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express();
+
+const page = `
+<html>
+<head>
+<title>Erik Hendrikson</title>
+</head>
+<body>
+<h1>Did you hear about the Italian Chef that died?</h1>
+<p>he pasta way</p>
+</body>
+</html>
+`
+app.get('/', function (request, response){
+    response.send(page)
+})
+
+const port = 3000
+
+app.listen(port, console.log(`listening on port ${port}`))
